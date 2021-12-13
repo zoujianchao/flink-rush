f02bc02abf5b0d7b1bbedfa35d9c98b54dac542e
zoujianchao <data_zjc@163.com>
2021-12-13 17:38:42 +0800

    初始化

---
+++ src/main/java/com/zjc/wc/WordCount.java
@@ -0,0 +1,14 @@
+package com.zjc.wc;
+
+/**
+ * @author : zoujianchao
+ * @version : 1.0
+ * @date : 2021/12/13
+ * @description : 批处理wc
+ */
+public class WordCount {
+    public static void main(String[] args) {
+        //创建执行环境
+        
+    }
+}
